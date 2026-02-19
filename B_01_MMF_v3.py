c7b5ebc33b10e69ec3b63ed42fcd4f594f040bbb
Yoda-Yoda-Yoda <chambersl3255@masseyhigh.school.nz>
2026-02-20 12:09:16 +1300

    Post raffle adjusted total / profit

---
+++ B_01_MMF_v3.py
@@ -0,0 +1,227 @@
+import pandas
+import random
+# Functions go here
+def make_statement(statement, decoration):
+    """Creates heading (3 lines), surrounding (2 lines) and
+    emphasised text / mid-heading (1 line). Only use emoji for
+    single line statements"""
+
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+
+    print(middle)
+
+
+
+def string_check(question, valid_ans=('yes', 'no'),
+                 num_letters=1):
+    """Checks that users enter the full word
+    or the 'n' letter/s of a word from a list of valid responses"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_ans:
+
+            # check if the response is the entire word
+            if response == item:
+                return item
+
+            # check if it's the 'n' letters
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_ans}")
+
+
+def instructions():
+    make_statement("Instructions", "ℹ️")
+
+    print('''
+    
+For each ticket holder enter ...
+- Their name
+- Their age
+- The payment method (cash / credit)
+
+The program will record the ticket sale and calculate the 
+ticket cost (and the profit).
+
+Once you have either sold all of the tickets or entered the 
+exit code ('xxx'), the program will display the ticket 
+sales information and writ the data to a text file.
+
+It will also choose one lucky ticket holder who wins the 
+draw (their ticket is free)
+
+    ''')
+
+def not_blank(question):
+    """Checks that a user input isn't blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry! this can't be blank. Please try again\n")
+
+
+def int_check(question):
+    """Checks that users enter an integer between two values"""
+
+
+    error = "Oops - please enter an integer"
+
+    while True:
+        response = input(question).lower()
+
+        # checks for the exit code
+        if response == "xxx":
+            return response
+
+        try:
+            # Change the response to an integer and check that it's more than zero
+            response = int(response)
+
+            return response
+
+        except ValueError:
+            print(error)
+
+
+# currency formatting function
+def currency(x):
+    """Formats numbers as currency ($#.##"""
+    return "${:.2f}".format(x)
+
+
+# Main routine goes here
+# Initialise ticket numbers
+MAX_TICKETS = 5
+tickets_sold = 0
+
+# initialise variables / non-default options for string checker
+payment_ans = ('cash', 'credit')
+
+
+# Ticket price list
+CHILD_PRICE = 7.50
+ADULT_PRICE = 10.50
+SENIOR_PRICE = 6.50
+
+# Credit card surcharge (currently 5%)
+CREDIT_SURCHARGE = 0.05
+
+# lists to hold ticket details
+all_names = []
+all_ticket_costs = []
+all_surcharges = []
+
+mini_movie_dict = {
+    'Name': all_names,
+    'Ticket Price': all_ticket_costs,
+    'Surcharge': all_surcharges
+}
+# Program main heading
+make_statement("Mini-Movie Fundraiser Program", "🍿")
+# Ask user if they want to see the instructions
+# display them if necessary
+print()
+want_instructions = string_check("Do you want to see the instructions? ")
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+# Loop to get name, age and payment details
+while tickets_sold < MAX_TICKETS:
+    print()
+    name = not_blank("Name: ")
+
+    # If name is exit code, break out of loop
+    if name == "xxx":
+        break
+
+    age = int_check("Age: ")
+    # Output error message / success message
+    if age < 12:
+        print(f"{name} is too young")
+        continue
+
+    # Child ticket price is $7.50
+    elif age < 16:
+        ticket_price = CHILD_PRICE
+    # Adult ticket is $10.50
+    elif age < 65:
+        ticket_price = ADULT_PRICE
+    elif age < 121:
+        ticket_price = SENIOR_PRICE
+    else:
+        print(f"{name} is too old ")
+        continue
+
+    # ask user for payment method (cash / credit / ca / cr)
+    pay_method = string_check("Payment method: ", payment_ans, 2)
+
+    if pay_method == "cash":
+        surcharge = 0
+
+    # if paying by credit, calculate surcharge
+    else:
+        surcharge = ticket_price * CREDIT_SURCHARGE
+
+    # add name, ticket cost amd surcharge
+    all_names.append(name)
+    all_ticket_costs.append(ticket_price)
+    all_surcharges.append(surcharge)
+
+    tickets_sold += 1
+
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+
+# Calculate the total payable fpr each ticket
+mini_movie_frame['Total'] = mini_movie_frame['Ticket Price'] + mini_movie_frame['Surcharge']
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+# Work out the total paid and total profit...
+total_paid = mini_movie_frame['Total'].sum()
+total_profit = mini_movie_frame['Profit'].sum()
+
+# choose random winner...
+winner = random.choice(all_names)
+
+# find index of winer (ie: position in list)
+winner_index = all_names.index(winner)
+print("Winner", winner, "list position", winner_index)
+
+# retrieve ticket price and surcharge
+winner_ticket_price = all_ticket_costs[winner_index]
+winer_surcharge = all_surcharges[winner_index]
+
+# find total won
+total_won = mini_movie_frame.at[winner_index, 'Total']
+profit_won = mini_movie_frame.at[winner_index, 'Profit']
+
+# Currency Formatting (uses currency function
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print(mini_movie_frame.to_string(index=False))
+print()
+print(f"Total Paid: ${total_paid:.2f}")
+print(f"Total Profit: ${total_profit:.2f}")
+
+
+# winer announcement
+print(f"The lucky winner is {winner}. Their ticket worth {total_won} is free!")
+print(f"Total Paid is now ${total_paid - profit_won:.2f}")
+print(f"Total Profit is now ${total_profit - profit_won:.2f}")
+
+if tickets_sold == MAX_TICKETS:
+    print(f"You have sold all the tickets (ie: {MAX_TICKETS} tickets")
+else:
+    print(f"You have sold {tickets_sold} / {MAX_TICKETS} tickets")
